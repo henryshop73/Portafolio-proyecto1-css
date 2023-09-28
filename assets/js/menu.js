6fff736b0f3a18e9b6789b0186db6b4dfaf9821f
henryshop73 <henrymoranfreelance@gmail.com>
2023-09-27 20:50:52 -0500

    Se agrega evento resize en menu.js para que se cierre el menú responsive al ser redimensionada la web

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -19,4 +19,14 @@ document.addEventListener("DOMContentLoaded", (evnt) => {
             icon_xmark.style.opacity = 0;
         }
     });
+
+    window.addEventListener("resize", () => {
+        let size = parseInt(document.body.clientWidth);
+
+        if(size <= 1060){
+            aside.classList.remove("layout__aside--visible");
+            icon_bars.style.opacity = 1;
+            icon_xmark.style.opacity = 0;
+        }
+    })
 });
